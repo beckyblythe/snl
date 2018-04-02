c0be7d7a019b3e4f2d0fb20f968b059a75bfe44d
Tatiana Prudnikova <prudnikova@ayrton.itb.pri>
2018-04-02 20:05:43 +0200

    better down and up hists

--- last_down_distr.py
+++ last_down_distr.py
@@ -17,6 +17,7 @@ for I_noise in I_noises:
 
                 with open('simulations/'+file_name, 'rb') as f:
                     results = get_simulation(file_name)
+                    _, saddle,_,_ = get_points(tau_n, Iapp)
     
                     n_last_down = np.array([n for neuron in results['n_last_down'] for n in neuron])
                     n_last_up = np.array([n for neuron in results['n_last_up'] for n in neuron])
@@ -24,16 +25,22 @@ for I_noise in I_noises:
 
                     plt.figure(figsize=(3., 3.))
                     plt.hist(n_last_down, bins = 30)
+                    plt.axvline(saddle[1], color = '.5')
+                    plt.axvline(np.median(n_last_down), color = 'b')
                     plt.xlabel('n')
                     plt.ylabel ('Distribution of n')
+                    plt.xlim((-.1,.7))
                     plt.savefig('down_distr/'+file_name+'.png', bbox_inches='tight') 
                     plt.show()
                     plt.close()
                     
                     plt.figure(figsize=(3., 3.))
-                    plt.hist(n_last_up, bins = 30)
+                    plt.hist(n_last_up, bins = 30, range = ((0,.003)))
+                    plt.axvline(saddle[1], color = '.5')
+                    plt.axvline(n_last_up.mean(), color = 'r')
                     plt.xlabel('n')
                     plt.ylabel ('Distribution of n')
+                    plt.xlim((0,.003))
                     plt.savefig('up_distr/'+file_name+'.png', bbox_inches='tight') 
                     plt.show()
                     plt.close()
