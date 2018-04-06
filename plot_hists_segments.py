36493af9a194f06f0c40846814fe58d7ea9bd652
Tatiana Prudnikova <prudnikova@brahmagupta.itb.pri>
2018-04-06 16:23:24 +0200

    working on plots and plotting

--- plot_hists_segments.py
+++ plot_hists_segments.py
@@ -1,19 +1,13 @@
 from brian2 import *
 from Neuron import *
 
-def plot_all_histograms(name):
-    plt.rcParams['figure.figsize'] = 12, 12  
-
-    tau_ns = [ .155, .1575,.16,.1625, .165]*ms
-    Iapps = [1.2,2.3,3.2,3.9,4.3]*uA
-    I_noises = [2,2.5,3]*uA
-    duration = 50*second
-    number = 10
-    
+def parameters_by_name(name):
     if name == 'segments':
         keys = ['time_up', 'time_down', 'time_above']
     if name == 'quiet_and_burst':
         keys = ['ISI_quiet', 'ISI_burst']
+    if name == 'burst':
+        keys = ['ISI_burst']
     if name == 'all':
         keys = ['ISI']
     if name == 'quiet_and_up':
@@ -27,6 +21,42 @@ def plot_all_histograms(name):
     if name == 'n_last_up':
         keys = [name]
         
+    return keys
+
+def plot_subplot(fig, file_name, name, idx_tau_n, idx_Iapp):
+    keys = parameters_by_name(name)
+    
+    ax = fig.add_subplot(5,5,5*(4 - idx_tau_n)+idx_Iapp +1)
+    
+    try:     
+       
+        results = get_simulation(file_name)
+        print(file_name)
+        intervals = results
+        flat_intervals = np.array([interval for neuron in intervals['ISI'] for interval in neuron])
+        mean = 5 # flat_intervals.mean()*1000
+        ax.xlim = ((0,5*mean))
+        for key in keys:
+            flat_intervals = np.array([interval for neuron in intervals[key] for interval in neuron])
+            ax.hist(flat_intervals*1000, normed = True, bins = 50,  alpha = .5, range = ((0,5*mean)), log = False, label = key)
+        if name == 'quiet_and_burst':
+            num_ISI = np.sum([len(neuron) for neuron in intervals['ISI']])
+            num_ISI_burst = np.sum([len(neuron) for neuron in intervals['ISI_burst']])
+            ax.set_title(str(int(num_ISI_burst/num_ISI*100))+'% ISI_burst')
+    except IOError:
+        ax.axis('off')
+        pass
+    
+
+def plot_all_histograms(name):
+    plt.rcParams['figure.figsize'] = 12, 12  
+
+    tau_ns = [ .155, .1575,.16,.1625, .165]*ms
+    Iapps = [1.2,2.3,3.2,3.9,4.3]*uA
+    I_noises = [2,2.5,3]*uA
+    duration = 50*second
+    number = 10
+          
     for I_noise in I_noises:
         
         fig = plt.figure()
@@ -34,26 +64,14 @@ def plot_all_histograms(name):
         for idx_tau_n, tau_n in enumerate(tau_ns):
             for idx_Iapp, Iapp in enumerate(Iapps):
                 file_name=str(duration/second)+' s  '+str(I_noise)+' '+str(tau_n)+'  '+str(Iapp)+' '+str(number)
-                ax = fig.add_subplot(5,5,5*(4 - idx_tau_n)+idx_Iapp +1)
-                print(file_name)
-                try:     
-                    results = get_simulation(file_name)
-                    intervals = results
-                    flat_intervals = np.array([interval for neuron in intervals['ISI'] for interval in neuron])
-                    mean = flat_intervals.mean()*1000
-                    ax.xlim = ((0,5*mean))
-                    for key in keys:
-                        flat_intervals = np.array([interval for neuron in intervals[key] for interval in neuron])
-                        ax.hist(flat_intervals*1000, normed = True, bins = 50,  alpha = .5, range = ((0,5*mean)), log = False, label = key)
-                        
-                except IOError:
-                    ax.axis('off')
-                    pass
+                plot_subplot(fig, file_name, name, idx_tau_n, idx_Iapp )
                 
-        ax = fig.add_subplot(5,5,1)
-        plt.legend(loc = 2)
+                
+        plt.legend(loc = 1)
+       
+        fig.text(0.5, -0.01, 'common X', ha='center')
+        fig.text(-0.01, 0.5, 'common Y', va='center', rotation='vertical')
         plt.tight_layout()
-        
-        plt.savefig('pictures_report/'+name+' '+str(I_noise)+'.png')
+        plt.savefig('pictures_report/'+name+' '+str(I_noise)+'.png', bbox_inches='tight')
         
 plot_all_histograms('quiet_and_burst')
--- ratio.py
+++ ratio.py
@@ -2,39 +2,40 @@ from Neuron import get_simulation
 from brian2 import *
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-plt.rcParams['figure.figsize'] = 9, 6
+plt.rcParams['figure.figsize'] = 12, 9
 
-tau_n = .153*ms
-Iapp = 1* uA #/cm**2
-I_noise = 2.5*uA
+
+tau_ns = [.165,.1625,.160,.1575,.155]*ms
+Iapps = [4.3,3.9,3.2,2.3,1.2]*uA
+I_noises = [2,2.5,3]*uA
 duration = 50000*ms
-
-v0=-30*mV
-n0=.05
+number = 10
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 ax.set_xlabel('tau_n (ms)')
 ax.set_ylabel('Iapp (uA)')
 ax.set_zlabel('fraction of burtst ISIs')
-ax.set_xticks([.155, .1575,.16,.1625, .165])
-ax.set_yticks([1.2,2.3,3.2,3.9,4.3,4.51])
-ax.view_init(10,180)
+ax.set_xticks([.165,.1625,.160,.1575,.155])
+ax.set_yticks([4.3,3.9,3.2,2.3,1.2])
+ax.view_init(10,-75)
+plt.gca().invert_xaxis()
 
-for tau_n in [ .155, .1575,.16,.1625, .165]*ms:
-    for Iapp in [1.2,2.3,3.2,3.9,4.3,4.51]*uA:
-        for duration in [10000,50000]*ms:
-
-            file_name=str(tau_n)+'  '+str(Iapp)+'  ('+str(v0)+', '+str(n0)+')  '+str(duration/second)+' s  '+str(I_noise)
-#            print(file_name)
+for I_noise, color in zip(I_noises,['b','r','g']):
+    for tau_n in tau_ns:
+        for Iapp in Iapps:
+            file_name=str(duration/second)+' s  '+str(I_noise)+' '+str(tau_n)+'  '+str(Iapp)+' '+str(number)
+           
             
             try:     
                 data = get_simulation(file_name)
+                print(file_name)
                 ISIs = [ISI for neuron in data['ISI'] for ISI in neuron ]
-                ISI_quiet = [ISI for neuron in data['ISI_quiet'] for ISI in neuron ]
+                ISI_quiet = [ISI for neuron in data['ISI_burst'] for ISI in neuron ]
                 ratio = len(ISI_quiet)/len(ISIs)
                 print(tau_n/ms,Iapp/uA, ratio)    
-                ax.scatter(tau_n/ms,Iapp/uA, ratio)
+                ax.scatter(tau_n/ms,Iapp/uA, ratio, color = color)
+#                ax.plot(Iapp/uA, ratio, 'r+', zdir='x', zs = 1.5)
                 
             
             except IOError:
